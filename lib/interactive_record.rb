70aaff6dc485076e2d825b979218a02846b4bfe5
Emily Lillie <emilylillie3@gmail.com>
2019-08-19 23:20:26 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -12,6 +12,6 @@ class InteractiveRecord
     sql = "pragma table_info('#{table_name}')"
     table_info = DB[:conn].execute(sql)
     column_names = []
-    table_info.each
+    table_info.each do |row| 
   
 end
